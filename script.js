5a7f141caef6fc5a958d59663a9d38df20c0f65f
nikhil bagal <nbagal02@gmail.com>
2019-10-27 14:33:10 +0530

    bugs fix for js code to add html layout

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const url = 'https://restcountries.eu/rest/v2/all';
 var info = {};
 
 fetch(url).then(responce => responce.json()).then(function(data){
-   // console.log(Object.getOwnPropertyNames(data[0]).filter(item => typeof data[item] === 'function'));
     for(var i=0;i<data.length;i++){
-        info[i] = data[i];
+        addWrapper(data[i]);
+        shortView(data[i]);
+        fullView(data[i]);
+        image(data[i],'.short-view');
+        headIng(data[i],'.short-view');
+        paraGraph(data[i],'.short-view');
     }
+    
+   // info = data;
     
   /*  console.log(Object.keys(data[0]));
     
     createElement('div','af',element);
     addHtml();*/
 });
-/*function createElement(elementType,classN,parrentElement){
-    var newElement = document.createElement(elementType);
-    newElement.className = classN;
-    parrentElement.appendChild(newElement);
-}
-function addHtml(){
+
+/*function addHtml(){
     var ele = document.querySelector('main .af');
     var country = info[0];
     ele.innerHTML += country['name'];
@@ -34,46 +36,33 @@ function createElement(elementType,classN,parrentElement,attrType,attrVal){
     parrentElement.appendChild(newElement);
 }
 function addWrapper(e){
-    createElement('div','country',element,'id',e['name']);
-    console.log(info);
-    console.log(e['name']);
+    createElement('div','country',element,'id',e['alpha2Code']);
 }
 function shortView(e){
-    var parentSelector = document.querySelector('main .container .country');
-   // console.log(parentSelector);
-    createElement('div','short-view',parentSelector,'id',e['alpha2code']+'-'+'short-view');
+    var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']);
+    var id = e['alpha2Code']+'-'+'short-view';
+    createElement('div','short-view',parentSelector,'id',id);
 }
 function fullView(e){
-    var parentSelector = document.querySelector('main .container .country');
-    createElement('div','full-view',parentSelector,'id',e['alpha2code']+'-'+'full-view');
+    var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']);
+    var id = e['alpha2Code']+'-'+'full-view';
+    createElement('div','full-view',parentSelector,'id',id);
 }
 function image(e,ele){
-    var parentSelector = document.querySelector('main .container .country'+' '+ele);
-   // console.log(parentSelector);
+    var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']+' '+ele);
     createElement('img','flag',parentSelector,'src',e['flag']);
-
 }
 function headIng(e,ele){
-    var parentSelector = document.querySelector('main .container .country'+' '+ele);
-    createElement('h3','heading',parentSelector,'id',e['alpha2code']+'-'+'name');
+    var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']+' '+ele);
+    createElement('h3','heading',parentSelector,'id',e['alpha2Code']+'-'+'name');
 }
 function paraGraph(e,ele){
-    var parentSelector = document.querySelector('main .container .country'+' '+ele);
+    var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']+' '+ele);
     createElement('p','para',parentSelector,'id','');
 }
 function init(){
-   
-    for(var i=0;i<info.length;i++){
-        console.log(info.length);
-        addWrapper(info[i]);
-        shortView(info[i]);
-        fullView(info[i]);
-        image(info[i],'.short-view');
-        headIng(info[i],'.short-view');
-        paraGraph(info[i],'.short-view');
-    }
-    
+
 }
 function addHtml(){
-
+    
 }
