ab6e5d279a3b642289a4bc07d78750eb3854802a
nikhil bagal <nbagal02@gmail.com>
2019-11-09 13:48:06 +0530

    minor changes to design

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let element = document.querySelector('main .container');
 const url = 'https://restcountries.eu/rest/v2/all';
 
-
 fetch(url).then(responce => responce.json()).then(function(data){
     for(var i=0;i<data.length;i++){
         addWrapper(data[i]);
@@ -29,17 +28,12 @@ fetch(url).then(responce => responce.json()).then(function(data){
         fullViewAddHtml(data[i],'p','currencies');
         fullViewAddHtml(data[i],'p','languages');
         buttonID(data[i],'.full-view');
-        
-    //    console.log(data[i]['region']);
     }
-    console.log(data[0]['name']);
-    console.log(data[0]['borders']);
     buttonAddHtml(data);
     search(data);
     viewChanger();
     backbutton();
     countryButton();
-    console.log(Object.keys(data[0]));
 })
 
 window.onload = init;
@@ -53,29 +47,23 @@ function createElement(elementType,classN,parrentElement,attrType,attrVal){
 function addWrapper(e){
     if(e['region'] === 'Asia'){
         var parentSelector = document.querySelector('main .container .asia');
-     //   console.log(parentSelector);
         createElement('div','country',parentSelector,'id',e['alpha2Code']);
     }else if(e['region'] === 'Europe'){
         var parentSelector = document.querySelector('main .container .europe');
-      //  console.log(parentSelector);
         createElement('div','country',parentSelector,'id',e['alpha2Code']);
     }else if(e['region'] === 'Africa'){
         var parentSelector = document.querySelector('main .container .africa');
-      //  console.log(parentSelector);
         createElement('div','country',parentSelector,'id',e['alpha2Code']);
     }else if(e['region'] === 'Americas'){
         var parentSelector = document.querySelector('main .container .america');
-     //   console.log(parentSelector);
         createElement('div','country',parentSelector,'id',e['alpha2Code']);
     }else if(e['region'] === 'Oceania'){
         var parentSelector = document.querySelector('main .container .oceania');
-      //  console.log(parentSelector);
         createElement('div','country',parentSelector,'id',e['alpha2Code']);
     }else {
         var parentSelector = document.querySelector('main .container .others');
         createElement('div','country',parentSelector,'id',e['alpha2Code']);
     }
-    
 }
 function shortView(e){
     var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']);
@@ -106,7 +94,6 @@ function addWrapperFullView(e){
     var parentSelector = document.querySelector('#'+e['alpha2Code']+ ' .full-view');
     createElement('div','img-btn-grp',parentSelector,'id','');
     createElement('div','para-btn-grp',parentSelector,'id','');
- //   console.log(parentSelector);
 }
 
 function imgFullView(e,ele){
@@ -137,9 +124,7 @@ function backButtonFullView(e){
 function buttonsFullView(e,ele){
     var parentSelector = document.querySelector('main .container '+'#'+e['alpha2Code']+' '+ele+' .neighbourCountries');
     parentSelector.innerHTML += '<h3>Border Countries: </h3>';
-   // console.log(parentSelector);
-   var nbrCtn = e['borders'];
-  // console.log(nbrCtn);
+    var nbrCtn = e['borders'];
   if(nbrCtn.length > 0){
     for(var i=0;i<nbrCtn.length;i++){
         createElement('button','btn',parentSelector,'value',nbrCtn[i]);
@@ -149,12 +134,11 @@ function buttonsFullView(e,ele){
 
 function buttonID(e,ele){
     var parentSelector = document.querySelectorAll('main .container '+'#'+e['alpha2Code']+' '+ele+' .neighbourCountries button');
-    var nbrCtn = parentSelector //.children;
+    var nbrCtn = parentSelector ;
     for(var i = 0;i<nbrCtn.length;i++){
         if(nbrCtn[i].tagName === 'BUTTON'){
             nbrCtn[i].setAttribute('id',e['borders'][i]);
         }
-        
     }
 }
 
@@ -192,7 +176,6 @@ function fullViewAddHtml(e,ele,key){
         }
     }else {
         var selector = document.querySelector('#'+e['alpha3Code']+'-full-view .para-btn-grp '+ele);
-      //  console.log(selector);
         selector.innerHTML += e['name'];
     }
 }
@@ -202,14 +185,10 @@ function buttonAddHtml(e){
     for(var i=0;i<e.length;i++){
         obj[e[i]['alpha3Code']] = e[i]['name'];
     }
-   // console.log(obj);
     for(var i=0;i<e.length;i++){
         var selector = document.querySelectorAll('#'+e[i]['alpha3Code']+'-full-view .para-btn-grp .neighbourCountries .btn');
-        //console.log(selector);
         for(var j=0;j<selector.length;j++){
-           // console.log(selector[j]);
            selector[j].innerHTML += obj[selector[j].id];
-         // console.log(obj[j.id]);
         }
     }
 }
@@ -217,18 +196,14 @@ function buttonAddHtml(e){
 function init(){
     themeChanger();
     filter();
-    
-  //  search();
 }
 
 function addHtml(e,ele,key){
     var selector = document.querySelector('#'+e['alpha2Code']+'-short-view '+ele);
-  //  console.log(selector);
     if(ele === 'h3'){
         selector.innerHTML += e[key];
     }else if(ele === 'p'){
         var paraSelector = document.querySelector('#'+e['alpha2Code']+'-short-view '+'#'+key);
-           // console.log(paraSelector);
         if(key === 'population'){
             paraSelector.innerHTML += '<b>Population</b>: '+e['population'];
         }else if(key === 'region'){
@@ -249,7 +224,6 @@ function themeChanger(){
         var shortViewSelector = document.querySelectorAll('main .container .country .short-view');
         var fullViewSelector = document.querySelectorAll('.country .full-view');
         var headerSelector = document.querySelector('main header .theme-head');
-       // console.log(headerSelector);
         for(var i of shortViewSelector){
             i.classList.toggle('active');
         }
@@ -260,18 +234,14 @@ function themeChanger(){
         headerSelector.classList.toggle('active');
         for(var i of fullViewSelector){
             i.classList.toggle('night-mode');
-        }
-        
-       // elementSelector.parentElement.classList.toggle('active');        
+        }     
     })
 }
 
 function filter(){
     var selectionSelector = document.querySelector('main header nav #region-selector');
    selectionSelector.addEventListener('input',function(event){
-      // console.log(event.target.value);
       var containerSelector = document.querySelector('main .container '+'.'+event.target.value);
-     // console.log(containerSelector.parentElement.children);
      var siblingSelector = containerSelector.parentElement.children;
      for(var i of siblingSelector){
          if(i.className !== event.target.value){
@@ -288,21 +258,16 @@ function search(data){
     var info = {} ;
     var key = {};
     var searchBoxSelector = document.querySelector('main header nav .searchBox input');
-   // console.log(searchBoxSelector);
     for(var i=0;i<data.length;i++){
         info[data[i]['name']] = data[i]['alpha2Code'];
 
     }
     searchBoxSelector.addEventListener('keyup',function(){
-       //console.log(event.target.value);
        var text;
        var regex;
        if(event.target.value !== ''){
         text = '^'+event.target.value;
         regex = new RegExp(text,'gi');
-       // console.log(regex.test(text));
-      //  console.log(text);
-       // console.log(regex);
        }else {
             var allCountrySelector = document.querySelectorAll('main .container .country');
             for(var i of allCountrySelector){
@@ -311,12 +276,8 @@ function search(data){
        }
        key = Object.keys(info);
        for(var i=0;i<key.length;i++){
-          // console.log(key[i]);
           if(regex.test(key[i]) === true){
-             // console.log(key[i]);
-           // console.log(info[key[i]]);
            var alphaCode = info[key[i]];
-          // console.log(alphaCode);
            var countrySelector =  document.querySelector('main .container '+'#'+alphaCode);
            var allCountrySelector = document.querySelectorAll('main .container .country');
            for(var i of allCountrySelector){
@@ -326,19 +287,15 @@ function search(data){
                    i.style.display = 'block';
                }
            }
-          //  console.log(countrySelector);
           }
        }
-       
     })
-    
 }
 
 function viewChanger(){
     var elementSelector = document.querySelectorAll('.country .short-view');
     for(var i of elementSelector){
         i.addEventListener('click',function(){
-         //   console.log(event.currentTarget.parentElement.id);
             var elementId = event.currentTarget.parentElement.id;
             var fullViewElement = document.querySelector('#'+elementId+' .full-view');
             fullViewElement.classList.add('active');
@@ -346,10 +303,6 @@ function viewChanger(){
             for(var i of smallViewSelector){
                 i.style.display = 'none';
             }
-          /*  var smallViewElement = document.querySelectorAll('.country .short-view');
-            for(var i of smallViewElement){
-                i.classList.add('')
-            }*/
         })
     }
 }
@@ -365,7 +318,6 @@ function backbutton(){
             }
         })
     }
-   // console.log(elementSelector);
 }
 
 function countryButton(){
